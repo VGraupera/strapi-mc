feb467df0a94b103037b083d1536d4fdfce231da
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-12-04 21:37:35 -0500

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,6 +14,7 @@ module.exports = [
       },
     },
   },
+  'strapi::poweredBy',
   {
     name: 'strapi::cors',
     config: {
@@ -22,7 +23,6 @@ module.exports = [
       origin: ['http://localhost:1337', 'https://strapi-production-c21a.up.railway.app']
     }
   },
-  'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
